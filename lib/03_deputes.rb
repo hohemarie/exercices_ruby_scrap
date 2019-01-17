8437fe6d9e9acc92f7a400686383386843bcad26
marie goudon <marie.goudon@laposte.net>
2019-01-17 23:52:18 +0100

    nom prenom adresse email scrapping

--- lib/03_deputes.rb
+++ lib/03_deputes.rb
@@ -5,11 +5,19 @@ require 'open-uri'
 def capturemail
 pagination = 10
 while pagination != 570 do
+	pagination = pagination.to_s
 	url_entier="https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&debut_deputes="+pagination+"#pagination_deputes"
 	doc = Nokogiri::HTML(open(url_entier))
-	adresse=doc.css("ann_mail")[0]
-	prenom = doc.css("title_normal").split(" ")[1]
-	nom = doc.css("title_normal").split(" ")[2]	
-	puts adresse, prenom, nom
+	adresse=doc.css("a[class='ann_mail']").text
+
+	#adresse1=adress.split(":")	
+	prenom = doc.css("h2[class='titre_normal']")
+	#nom = doc.css("title_normal").split(" ")[2]	
+	puts adresse#, prenom #nom
+	puts prenom
+	pagination = pagination.to_i
+	pagination+=10
 end
 end
+
+capturemail
