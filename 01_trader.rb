78d2dce0ceff13274e5170742eccf91d6098e71d
marie goudon <marie.goudon@laposte.net>
2019-01-17 22:18:54 +0100

    création de deux dossiers

--- 01_trader.rb
+++ 01_trader.rb
@@ -3,7 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 # definition d'une fonction 
-def capture	
+def capture(devise_rentree)
 	puts "veuillez patienter..."
 	url = "https://coinmarketcap.com/all/views/all/"
 	prix = [] # on fait un tableau des prix
@@ -36,9 +36,10 @@ prix.each do |val|
 	x+=w
 	pri << x
 end
-Hash[noms.zip(pri)].each do |key,value|#j'associe les differents champs du tableau
-	puts key+" => "+value
-end 
-
+#Hash[noms.zip(pri)].each do |key,value|#j'associe les differents champs du tableau
+#	puts key+" => "+value
+#end 
+my_hash = Hash[noms.zip(pri)]
+return my_hash[devise_rentree]
 end
 capture
